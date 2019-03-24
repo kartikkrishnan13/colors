cbe3025dddb9a58df5b01798a9e7baa0f8c1a9f7
kartikkrishnan13 <47325112+kartikkrishnan13@users.noreply.github.com>
2019-03-25 00:47:52 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ var curColor = "";
 var app = angular.module("myApp",[]);
 
 app.controller("myCtrl",function($scope) {
+  alert("You can edit the RGB values.");
+  alert("You can change the RGB values using the range slider.");
+  alert("You can click on the text 'Closest Color: ...' to update the RGB value to that color.");
   $scope.red = 255;
   $scope.green = 255;
   $scope.blue = 255;
@@ -22,10 +25,24 @@ app.controller("myCtrl",function($scope) {
     }
     else {
       $scope.colorName = "None";
-      $scope.rgb = parseInt(curColor[0].slice(1, 3), 16) + ", " + parseInt(curColor[0].slice(3, 5), 16) + ", " + parseInt(curColor[0].slice(5, 7), 16);
+      $scope.nRed = parseInt(curColor[0].slice(1, 3), 16);
+      $scope.nGreen = parseInt(curColor[0].slice(3, 5), 16);
+      $scope.nBlue = parseInt(curColor[0].slice(5, 7), 16);
+      $scope.rgb = $scope.nRed + ", " + $scope.nGreen + ", " + $scope.nBlue;
       $scope.nearestColor = curColor[0] + " => " + curColor[1] + " (" + $scope.rgb + ")";
       $scope.exact = false;
     }
+  }
+  $scope.modifyValue = function() {
+    $scope.red = $scope.nRed;
+    $scope.green = $scope.nGreen;
+    $scope.blue = $scope.nBlue;
+    $scope.toast = "Color changed to " + curColor[1];
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function() {
+      x.className = x.className.replace("show", "");
+    }, 3000);
   }
   $scope.$watchGroup(['red', 'green', 'blue'], $scope.update);
   $scope.update();
